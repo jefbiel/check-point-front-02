684860deff38f4ea79af3775638a4efd359c87ec
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 10:35:30 -0300

    web-front

---
+++ series.js
@@ -0,0 +1,7 @@
+const my3FavoriteTVShows = ["The Dragon Prince", "House of the Dragon","The Big Bang Theory"];
+
+if (my3FavoriteTVShows.length === 3){
+    console.log("true")
+} else {
+    console.log("false")
+}
