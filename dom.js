55bdfb267fe18a6e0482d73c73d89535161d1779
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 11:49:49 -0300

    Java

---
+++ dom.js
@@ -0,0 +1,8 @@
+//criando variaveis
+const tirarDiv = document.querySelector(".tirarDiv")
+const voltarDiv = document.querySelector(".voltarDiv")
+const card = document.querySelector("card")
+
+tirarDiv.addEventListener("click", function(){
+    alert("Olá")
+})
---
+++ funcao.js
@@ -0,0 +1,17 @@
+function teste() {
+    console.log("Oláa Coraline")
+}
+
+function meuNome(name){
+    console.log(`OlÁ ${name}`)
+}
+
+meuNome("Gabriel")
+meuNome("Jeb")
+meuNome("Ezram")
+
+function soma(agr1, agr2) {
+    return `A soma ´3: ${arg1 + agr2}`;
+}
+
+console.log(soma(4,7))
